793b723ae1e6e62f6e4151873e04d05b6d118ce8
Figo <farag.muhammad63@gmail.com>
2019-08-31 05:31:08 +0200

    final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +15,53 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftBtn = 1;
+  int rightBtn = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+
+              onPressed: () {
+                setState(() {
+                  throwDice();
+                });
+              },
+              child: Image.asset('images/dice$leftBtn.png'),
+
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    throwDice();
+                  });
+                },
+                child: Image.asset('images/dice$rightBtn.png')
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  void throwDice() {
+    leftBtn = new Random().nextInt(6)+ 1;
+    rightBtn = new Random().nextInt(6)+ 1;
   }
 }
+
+
+
+
